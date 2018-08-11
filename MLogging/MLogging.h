6ed11b9782595b9d55e622605104d46801453392
Mohanish Bharaskar <mohanishbharaskar@gmail.com>
2018-08-11 20:50:50 +0530

    Initial Commit

---
+++ MLogging/MLogging.h
@@ -0,0 +1,19 @@
+//
+//  MLogging.h
+//  MLogging
+//
+//  Created by Mohanish Bharaskar on 11/08/18.
+//  Copyright © 2018 Mohanish Bharaskar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MLogging.
+FOUNDATION_EXPORT double MLoggingVersionNumber;
+
+//! Project version string for MLogging.
+FOUNDATION_EXPORT const unsigned char MLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLogging/PublicHeader.h>
+
+
